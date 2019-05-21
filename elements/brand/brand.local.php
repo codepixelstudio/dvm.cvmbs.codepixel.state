e2d1d54b4168beedceb57381ea0ffdf57bef9d3b
codepixelstudio <codepixel@codepixelstudio.com>
2019-05-21 15:34:46 -0600

    add DVM site identifier

--- elements/brand/brand.local.php
+++ elements/brand/brand.local.php
@@ -1,28 +1,8 @@
-<?php
-
-    $site_title  = get_field( 'site_title', 'options' );
-    $title_break = get_field( 'site_title_line_break', 'options' );
-    $site_line_1 = get_field( 'site_title_line_1', 'options' );
-    $site_line_2 = get_field( 'site_title_line_2', 'options' );
-
-?>
 
 <!-- local.brand -->
-<span id="brand-local">
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>" id="brand-local">
 
-    <?php
+    Doctor of Veterinary<br />Medicine Program
 
-        if ( $title_break[ 0 ] == 'break' ) {
-
-            echo $site_line_1 . '<br />' . $site_line_2;
-
-        } else {
-
-            echo $site_title;
-
-        }
-
-    ?>
-
-</span>
+</a>
 <!-- END local.brand -->
